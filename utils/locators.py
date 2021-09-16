1c1d6d97b54e7b0839e805387e8c54adfe36c11a
Alexey <podzakon11@gmail.com>
2021-09-16 10:25:43 +0300

    changed xpaths

--- utils/locators.py
+++ utils/locators.py
@@ -2,21 +2,21 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
-    SIGN_IN = (By.XPATH, "/html/body/header/div/div/div/a[2]")
+    SIGN_IN = (By.XPATH, "//a[@data-action='sign in']")
 
 
 class GmailLoginPageLocators(object):
-    EMAIL_INPUT = (By.XPATH, "//*[@id='identifierId']")
-    PASSWORD_INPUT = (By.XPATH, "//*[@id='password']/div[1]/div/div[1]/input")
+    EMAIL_INPUT = (By.XPATH, "//input[@type='email']")
+    PASSWORD_INPUT = (By.XPATH, "//input[@type='password']")
     CONTINUE_BUTTON_LOGIN = (By.XPATH, "//*[@id='identifierNext']/div/button")
     CONTINUE_BUTTON_PASSWORD = (By.XPATH, "//*[@id='passwordNext']/div/button")
-    SEARCH_INPUT = (By.XPATH, "//*[@id='gs_lc50']/input[1]")
+    SEARCH_INPUT = (By.XPATH, "//input[@name='q']")
 
 
 class GmailMailboxPageLocators(object):
-    TO_ADDR = (By.NAME, "to")
-    SUBJECT = (By.NAME, "subjectbox")
-    EMAIL_LINK = (By.CSS_SELECTOR, "div[role='link']")
+    TO_ADDR = (By.XPATH, "//textarea[@name='to']")
+    SUBJECT = (By.XPATH, "//input[@name='subjectbox']")
+    EMAIL_LINK = (By.XPATH, "//div[@role='link']")
     SELECT_ALL = (
         By.XPATH,
         "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div/div/div[1]/div/div[1]/span",
